9bc2676e156993cc25fcf80fcd0426de48b38225
eloy98g <eloygomez132@gmail.com>
2021-04-16 20:41:54 +0200

    hero animation added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import gsap from 'gsap';
+import '../styles/components/Hero.scss';
 
 const Hero = () => {
+  const tl = gsap.timeline({ defaults: { ease: 'power1.out' } });
+
+  useEffect(() => {
+    tl.to('.text', { y: '0%', duration: 1, stagger: 0.25 });
+    tl.to('.slider', { y: '-100%', duration: 1.5, delay: 0.5 });
+    tl.to('.intro', { y: '-100%', duration: 1 }, '-=1');
+  
+    tl.fromTo('nav', { opacity: 0 }, { opacity: 1, duration: 1 });
+    tl.fromTo('.big-text', { opacity: 0 }, { opacity: 1, duration: 1 }, '-=1');
+  })
   return (
     <div className="hero">
       <div className="landing">
         <h1 className="name">Eloy Gómez</h1>
-        <h2 className="bullshit">Moble & Web Developer</h2>
+        <h1 className="bullshit">Mobile & Web Developer</h1>
       </div>
       <div className="intro">
         <div className="intro-text">
