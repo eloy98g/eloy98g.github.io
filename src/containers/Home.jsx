b146fa9d32ece737dcf078a84c184aa087c85771
eloy98g <eloygomez132@gmail.com>
2021-06-26 20:17:48 +0200

    tonto

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -19,7 +19,7 @@ import '../styles/containers/Home.scss';
 const Home = () => {
   return (
     <div className="home">
-      {/* <Hero /> */}
+      <Hero />
       <About />
       <TimeLine />
       <Projects />
