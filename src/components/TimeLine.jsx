b15be0e8ae6dc4012d049b5f2ec23415740e34cc
eloy98g <eloygomez132@gmail.com>
2021-08-10 20:13:35 +0200

    timeline hover bug fixed

--- src/components/TimeLine.jsx
+++ src/components/TimeLine.jsx
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 
 // Widgets
 import HoverPopup from '../widgets/HoverPopup';
-import timelineAnimation from '../animations/TimelineAnimation'
+import timelineAnimation from '../animations/TimelineAnimation';
 // Assets
 import uco from '../assets/uco.png';
 import clapfy from '../assets/clapfy.png';
@@ -12,10 +12,9 @@ import labora from '../assets/labora.png';
 // Styles
 import '../styles/components/TimeLine.scss';
 
-
 const TimeLine = () => {
   useEffect(() => {
-    timelineAnimation()
+    timelineAnimation();
   });
 
   return (
@@ -27,39 +26,39 @@ const TimeLine = () => {
             <div className="uco ybar">
               <a href="http://www.uco.es/" className="link" target="blank">
                 <img className="img" src={uco} alt="uco" />
+                <HoverPopup
+                  vertical
+                  title="UCO"
+                  description="Universidad de Córdoba"
+                  resume="Ingeniería informática en la UCO con mención en computadores"
+                  trigger="uco"
+                />
               </a>
               <p className="text">2016</p>
-              <HoverPopup
-                vertical
-                title="UCO"
-                description="Universidad de Córdoba"
-                resume="Ingeniería informática en la UCO con mención en computadores"
-                trigger="uco"
-              />
             </div>
             <div className="clapfy ybar">
               <a href="https://clapfy.es/" className="link" target="blank">
                 <img className="img" src={clapfy} alt="clapfy" />
+                <HoverPopup
+                  title="Clapfy"
+                  description="Madrid. Consultora b2b"
+                  resume="Tras una fase de formación frontend a nivel profesional, trabajé con un equipo en el que apliqué metodologías ágiles tanto en diseño como en desarrollo de proyectos en producción"
+                  trigger="clapfy"
+                />
               </a>
               <p className="text">Oct 20 - Abr 21</p>
-              <HoverPopup
-                title="Clapfy"
-                description="Madrid. Consultora b2b"
-                resume="Tras una fase de formación frontend a nivel profesional, trabajé con un equipo en el que apliqué metodologías ágiles tanto en diseño como en desarrollo de proyectos en producción"
-                trigger="clapfy"
-              />
             </div>
             <p className="labora ybar">
               <a href="https://www.labora.app/" className="link" target="blank">
                 <img className="img" src={labora} alt="labora" />
+                <HoverPopup
+                  title="Labora"
+                  description="Madrid. Startup"
+                  resume="Junto al equipo de Clapfy, comenzamos a desarrollar un producto propio destinado a facilitar al máximo las contrataciones de las empresas"
+                  trigger="labora"
+                />
               </a>
               <p className="text">Abr 21 - now</p>
-              <HoverPopup
-                title="Labora"
-                description="Madrid. Startup"
-                resume="Junto al equipo de Clapfy, comenzamos a desarrollar un producto propio destinado a facilitar al máximo las contrataciones de las empresas"
-                trigger="labora"
-              />
             </p>
           </div>
         </div>
